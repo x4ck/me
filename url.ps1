e693db02fcaac5fbc9448f8d687b0dd6ac323af3
x4ck <63238374+x4ck@users.noreply.github.com>
2020-04-16 08:46:52 -0400

    Delete url.ps1

--- url.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module BitsTransfer
-Start-BitsTransfer -Source "https://github.com/x4ck/me/raw/master/ps.zip" -Destination "c:\ps.zip"
-Expand-Archive c:\ps.zip -DestinationPath c:\powershell
-cd c:\powershell\
-.\run.ps1
